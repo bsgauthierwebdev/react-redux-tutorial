2f80e12bb2b490f61b716814d923296461b2836e
Brent Gauthier <cajundrummer@gmail.com>
2024-04-02 21:21:46 -0400

    Build basic counter app

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+    increment, 
+    decrement,
+    reset,
+    incrementByAmount
+} from './counterSlice'
+import { useState } from 'react'
+
+const Counter = () => {
+    const count = useSelector((state) => state.counter.count)
+    const dispatch = useDispatch()
+
+    const [incrementAmt, setIncrementAmt] = useState(0)
+
+    const addValue = Number(incrementAmt) || 0
+
+    const resetAll = () => {
+        setIncrementAmt(0)
+        dispatch(reset())
+    }
+
+  return (
+    <section>
+        <p>{count}</p>
+        <div>
+            <button onClick = {() => dispatch(increment())}>+</button>
+            <button onClick = {() => dispatch(decrement())}>-</button>
+        </div>
+
+        <input 
+            type="text"
+            value = {incrementAmt}
+            onChange = {(e) => setIncrementAmt(e.target.value)} 
+        />
+
+        <div>
+            <button onClick = {() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+            <button onClick = {resetAll}>Reset</button>
+        </div>
+    </section>
+  )
+}
+
+export default Counter
